7a9e593a9e4b7f134726b0b5f92f044b3536dc7b
Mijail Hernandez <mijailhernandez@gmail.com>
2021-02-22 20:19:32 -0500

    added primary font famly

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['commissioner'],
+      },
+      colors: {
+        button: 'hsl(176, 50%, 47%)',
+        buttonhover: 'hsl(176, 72%, 28%)',
+        darkgray: 'hsl(0, 0%, 48%)',
+      },
       zIndex: {
         '-10': '-10',
       }
