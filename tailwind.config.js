febea2934aca8174b4495421f31315d8690b8e15
Mijail Hernandez <mijailhernandez@gmail.com>
2021-02-27 01:23:02 -0500

    Added attribution.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { custom: ['commissioner'], },
-      colors: { 'cyan-100': 'hsl(176, 50%, 47%)', 'cyan-200': 'hsl(176, 72%, 28%)', darkgray: 'hsl(0, 0%, 48%)', },
+      colors: {
+        'cyan-100': 'hsl(176, 50%, 47%)',
+        'cyan-200': 'hsl(176, 72%, 28%)',
+        darkgray: 'hsl(0, 0%, 48%)',
+        link: 'hsl(228, 45%, 44%)',
+      },
       zIndex: { '-10': '-10', },
       width: { 'card-mb': '87%', }
     },
