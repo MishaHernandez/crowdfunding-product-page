40daa6a6a74e60c07ae188a3e48afdeccea945c4
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-03 03:46:12 -0500

    added modals functionality - mobile.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,26 +1,33 @@
 const menuIcon = document.getElementById("menu");
-const body = document.getElementsByTagName("body");
 const btnBack = document.getElementById("btnBack");
 const modalDeault = document.getElementById("modalDefault");
 const closeModalSuccess = document.getElementById("closeModalSuccess");
+const overlay = document.getElementById("overlay");
 
 menuIcon.addEventListener("click", ()=> {
     const menu = menuIcon.nextElementSibling;
     menu.classList.toggle("hidden");
 
     if (menu.classList.contains("hidden")) {
-        body.classList.add("cover");
+        menuIcon.setAttribute("src", "images/icon-hamburger.svg");
+        overlay.classList.add("hidden");
+    } else {
+        menuIcon.setAttribute("src", "images/icon-close-menu.svg");
+        overlay.classList.remove("hidden");
     }
 })
 
 btnBack.addEventListener("click", ()=> {
     modalDeault.classList.remove("hidden");
+    overlay.classList.remove("hidden");
 })
 
 modalDeault.firstElementChild.addEventListener("click", ()=> {
     modalDeault.classList.add("hidden");
+    overlay.classList.add("hidden");
 })
 
 closeModalSuccess.addEventListener("click", ()=> {
     closeModalSuccess.parentElement.classList.add("hidden");
+    overlay.classList.add("hidden");
 })
