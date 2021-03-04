75f70d10c9b68ee239587e8a391bc41088f95fa8
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-04 07:52:33 -0500

    fixed modal functionality.

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,8 @@ const btnBack = document.getElementById("btnBack");
 const modalDeault = document.getElementById("modalDefault");
 const closeModalSuccess = document.getElementById("closeModalSuccess");
 const overlay = document.getElementById("overlay");
+const modalSuccess = document.getElementById("modalSuccess");
+const products = document.getElementById("products");
 
 menuIcon.addEventListener("click", ()=> {
     const menu = menuIcon.nextElementSibling;
@@ -20,14 +22,35 @@ menuIcon.addEventListener("click", ()=> {
 btnBack.addEventListener("click", ()=> {
     modalDeault.classList.remove("hidden");
     overlay.classList.remove("hidden");
+    menuIcon.classList.remove("z-20");
+    document.querySelector("header img").classList.remove("z-20");
 })
 
 modalDeault.firstElementChild.addEventListener("click", ()=> {
     modalDeault.classList.add("hidden");
     overlay.classList.add("hidden");
+    menuIcon.classList.add("z-20");
+    document.querySelector("header img").classList.add("z-20");
+})
+
+products.addEventListener("click", (e)=> {
+    if (e.target.matches("button")) {
+        overlay.classList.remove("hidden");
+        modalDeault.classList.remove("hidden");
+    }        
+})
+
+modalDeault.addEventListener("click", (e) => {
+    if (e.target.matches("button")) {
+        overlay.classList.remove("hidden");
+        modalSuccess.classList.remove("hidden");
+        modalDeault.classList.add("hidden");
+    }
 })
 
 closeModalSuccess.addEventListener("click", ()=> {
     closeModalSuccess.parentElement.classList.add("hidden");
     overlay.classList.add("hidden");
+    menuIcon.classList.add("z-20");
+    document.querySelector("header img").classList.add("z-20");
 })
