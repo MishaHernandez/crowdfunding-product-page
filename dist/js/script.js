24838d4c4e57317d8bb3fa6a550d73c963839e65
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-11 23:23:12 -0500

    Optimized javascript code.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,68 +1,80 @@
 const menuIcon = document.getElementById("menu");
+const cta = document.getElementById("cta");
 const btnBack = document.getElementById("btnBack");
-const modalDeault = document.getElementById("modalDefault");
+const modalDefault = document.getElementById("modalDefault");
 const closeModalSuccess = document.getElementById("closeModalSuccess");
 const overlay = document.getElementById("overlay");
-const modalSuccess = document.getElementById("modalSuccess");
 const products = document.getElementById("products");
 
-document.getElementById("cta").addEventListener("click", e => {
-    if (e.target.id === "bookmark" && e.target.lastElementChild.classList.contains("lg:text-darkgray")) {
-        e.target.firstElementChild.firstElementChild.children[0].setAttribute("fill", "hsl(176, 50%, 47%)");
-        e.target.firstElementChild.firstElementChild.children[1].setAttribute("fill", "#fff");
-        e.target.lastElementChild.classList.replace("lg:text-darkgray", "lg:text-cyan-100");
-        e.target.lastElementChild.textContent = "Bookmarked";
-    } else if (e.target.id === "bookmark" && e.target.lastElementChild.classList.contains("lg:text-cyan-100")) {
-        e.target.firstElementChild.firstElementChild.children[0].setAttribute("fill", "#2F2F2F");
-        e.target.firstElementChild.firstElementChild.children[1].setAttribute("fill", "#B1B1B1");
-        e.target.lastElementChild.classList.replace("lg:text-cyan-100", "lg:text-darkgray");
-        e.target.lastElementChild.textContent = "Bookmark";
-    }
-})
-
-menuIcon.addEventListener("click", ()=> {
+// ---------------------------------------------MENU MOBILE---------------------------------------------
+function menuState(url) {
+    menuIcon.setAttribute("src", url);
+    overlay.classList.toggle("hidden");
+}
+menuIcon.addEventListener("click", () => {
     const menu = menuIcon.nextElementSibling;
+    const logo = document.querySelector("header img");
+    
+    logo.classList.toggle("z-20");
+    menuIcon.classList.toggle("z-20");
     menu.classList.toggle("hidden");
 
     if (menu.classList.contains("hidden")) {
-        menuIcon.setAttribute("src", "images/icon-hamburger.svg");
-        overlay.classList.add("hidden");
+        menuState("images/icon-hamburger.svg");
     } else {
-        menuIcon.setAttribute("src", "images/icon-close-menu.svg");
-        overlay.classList.remove("hidden");
+        menuState("images/icon-close-menu.svg");
     }
 })
 
-btnBack.addEventListener("click", ()=> {
-    modalDeault.classList.remove("hidden");
-    modalDeault.querySelectorAll("div.px-6.py-5.border-t-2").forEach(elem => elem.classList.add("hidden"));
-    overlay.classList.remove("hidden");
-    menuIcon.classList.remove("z-20");
-    document.querySelector("header img").classList.remove("z-20");
+// ---------------------------------------------CTA SECTION---------------------------------------------
+// bookmark button
+function bookmarkState(toggle, roundButtonColor, iconColor, textColor1, textColor2, message) {
+    toggle.firstElementChild.firstElementChild.children[0].setAttribute("fill", roundButtonColor);
+    toggle.firstElementChild.firstElementChild.children[1].setAttribute("fill", iconColor);
+    toggle.lastElementChild.classList.replace(textColor1, textColor2);
+    toggle.lastElementChild.textContent = message;
+}
+
+cta.addEventListener("click", e => {
+    const textColor = "lg:text-darkgray";
+    const textColorActive = "lg:text-cyan-200";
+    const roundButtonColor = "#2F2F2F";
+    const roundButtonColorActive = "hsl(176, 72%, 28%)";
+    const iconColor = "#B1B1B1";
+    const iconColorActive = "#fff";
+    
+    // bookmark button
+    if (e.target.id === "bookmark") {
+        const toggle = e.target;
+        const toggleText = toggle.lastElementChild;
+        
+        if (toggleText.classList.contains("lg:text-darkgray")) {
+            bookmarkState(toggle, roundButtonColorActive, iconColorActive, textColor, textColorActive, "Bookmarked");
+
+        } else if (toggleText.classList.contains("lg:text-cyan-200")) {
+            bookmarkState(toggle, roundButtonColor, iconColor, textColorActive, textColor, "Bookmark");
+        }
+        
+    // back this project button
+    } else if (e.target.id === "btnBack") {
+        overlay.classList.remove("hidden");
+        modalDefault.classList.remove("hidden");
+        
+        menuIcon.classList.remove("z-20");
+        document.querySelector("header img").classList.remove("z-20");
+    }
 })
 
-// menu icon
-modalDeault.firstElementChild.addEventListener("click", ()=> {
-    modalDeault.classList.add("hidden");
-    overlay.classList.add("hidden");
-    menuIcon.classList.add("z-20");
-    document.querySelector("header img").classList.add("z-20");
-    modalDeault.querySelectorAll("section article").forEach(elem => {
-        elem.className = "relative mb-6 border-gray-300 card-modal lg:pt-2 lg:rounded-lg";
-        elem.lastElementChild.classList.replace("h-32", "h-0");
-        elem.lastElementChild.classList.replace("px-6", "px-0");
-        elem.lastElementChild.classList.replace("py-5", "py-0");
-        elem.lastElementChild.classList.replace("border-t-2", "border-t-0");
-        elem.querySelector("span.w-3.h-3").classList.add("hidden");
-    });
-})
-
-products.addEventListener("click", (e)=> {
+// ---------------------------------------------MAIN SECTION---------------------------------------------
+// SELECT REWARD
+products.addEventListener("click", (e) => {
     if (e.target.matches("a.button")) {
         overlay.classList.remove("hidden");
-        modalDeault.classList.remove("hidden");
+        modalDefault.classList.remove("hidden");
+        
+        // SELECT PRODUCT (from main section)
+        const modalCardSelected = modalDefault.querySelectorAll(`section article:nth-of-type(${e.target.dataset.card})`)[0];
 
-        const modalCardSelected = modalDeault.querySelectorAll(`section article:nth-of-type(${e.target.dataset.card})`)[0];
         modalCardSelected.className = "relative mb-6 border-2 border-cyan-100 card-modal lg:pt-2 lg:rounded-lg";
         modalCardSelected.lastElementChild.classList.replace("h-0", "h-32");
         modalCardSelected.lastElementChild.classList.replace("px-0", "px-6");
@@ -72,49 +84,72 @@ products.addEventListener("click", (e)=> {
     }
 })
 
-modalDeault.addEventListener("click", (e) => {
-    if (e.target.matches("button")) {
-        document.getElementById("amount").textContent = "$" + (parseInt((document.getElementById("amount").textContent).replace(/,|\$/gi,"")) + parseInt(e.target.previousElementSibling.firstElementChild.value)).toLocaleString();
-        document.getElementById("backers").textContent = (parseInt((document.getElementById("backers").textContent).replace(",","")) + 1).toLocaleString();
-        // console.log((parseInt((document.getElementById("amount").textContent).replace(/,|\$/gi, "")) * 100) / 100000);
-        document.getElementById("progressbar").style.width = ((parseInt((document.getElementById("amount").textContent).replace(/,|\$/gi, "")) * 100) / 100000) + "%";
+// ------------------------------------------------MODAL------------------------------------------------
+function resetDefaultModal() {
+     modalDefault.querySelectorAll("article").forEach(elem => {
+         elem.className = "relative mb-6 border-gray-300 card-modal lg:pt-2 lg:rounded-lg";
+        //  reset pledge sections
+         elem.lastElementChild.classList.replace("h-32", "h-0");
+         elem.lastElementChild.classList.replace("px-6", "px-0");
+         elem.lastElementChild.classList.replace("py-5", "py-0");
+         elem.lastElementChild.classList.replace("border-t-2", "border-t-0");
+        //  hidden dot
+         elem.querySelector("span.w-3.h-3").classList.add("hidden");
+     });
+}
+function updateProgressBar(inputPledge) {
+    let amount = parseInt((document.getElementById("amount").textContent).replace(/,|\$/gi, ""));
+    let backers = parseInt((document.getElementById("backers").textContent).replace(",", ""));
+    let progressbar = document.getElementById("progressbar");
 
+    document.getElementById("amount").textContent = "$" + (amount + parseInt(inputPledge)).toLocaleString();
+    document.getElementById("backers").textContent = (backers + 1).toLocaleString();
+    progressbar.style.width = ((amount * 100) / 100000) + "%";
+}
+
+// CLOSE MODAL
+closeModalDefault.addEventListener("click", () => {
+    modalDefault.classList.add("hidden");
+    overlay.classList.add("hidden");
+    resetDefaultModal();
+})
+
+// MAIN MODAL
+modalDefault.addEventListener("click", (e) => {
+    // CONTINUE BUTTON
+    if (e.target.matches("button")) {
+        const modalSuccess = document.getElementById("modalSuccess");
+        const inputPledge = e.target.previousElementSibling.firstElementChild.value;
+        
+        updateProgressBar(inputPledge);
+        modalDefault.classList.add("hidden");
         overlay.classList.remove("hidden");
         modalSuccess.classList.remove("hidden");
-        modalDeault.classList.add("hidden");
-        modalDeault.querySelectorAll("section article").forEach(elem => {
-            elem.className = "relative mb-6 border-gray-300 card-modal lg:pt-2 lg:rounded-lg";
-            elem.lastElementChild.classList.replace("h-32", "h-0");
-            elem.lastElementChild.classList.replace("px-6", "px-0");
-            elem.lastElementChild.classList.replace("py-5", "py-0");
-            elem.lastElementChild.classList.replace("border-t-2", "border-t-0");
-            elem.querySelector("span.w-3.h-3").classList.add("hidden");
-        });
-
+        resetDefaultModal();
+    
+    // SELECT PRODUCT (INPUT RADIO)
     } else if (e.target.matches("input[type='radio']")) {
-        modalDeault.querySelectorAll("section article").forEach(elem => elem.className = "relative mb-6 border-gray-300 card-modal lg:pt-2 lg:rounded-lg");
-        modalDeault.querySelectorAll("input[type='radio']").forEach(elem => elem.nextElementSibling.nextElementSibling.firstElementChild.classList.add("hidden"));
-        e.target.parentElement.parentElement.parentElement.className = "relative mb-6 border-2 border-cyan-100 card-modal lg:pt-2 lg:rounded-lg";
-        e.target.parentElement.querySelector("span.w-3.h-3").classList.remove("hidden");
+        resetDefaultModal();
         
+        // selected product card
         if (e.target.checked) {
-            modalDeault.querySelectorAll("div.pledge").forEach(elem => {
-                elem.classList.replace("h-32", "h-0");
-                elem.classList.replace("px-6", "px-0");
-                elem.classList.replace("py-5", "py-0");
-                elem.classList.replace("border-t-2", "border-t-0");
-            });
-            e.target.parentElement.parentElement.nextElementSibling.classList.replace("h-0", "h-32");
-            e.target.parentElement.parentElement.nextElementSibling.classList.replace("border-t-0", "border-t-2");
-            e.target.parentElement.parentElement.nextElementSibling.classList.replace("px-0", "px-6");
-            e.target.parentElement.parentElement.nextElementSibling.classList.replace("py-0", "py-5");
+            let pledgeSection = e.target.parentElement.parentElement.nextElementSibling;
+            const card = e.target.parentElement.parentElement.parentElement;
+            const dot = e.target.parentElement.querySelector("span.w-3.h-3");
+
+            card.className = "relative mb-6 border-2 border-cyan-100 card-modal lg:pt-2 lg:rounded-lg";
+            dot.classList.remove("hidden");
+            // selected pleged section
+            pledgeSection.classList.replace("h-0", "h-32");
+            pledgeSection.classList.replace("px-0", "px-6");
+            pledgeSection.classList.replace("py-0", "py-5");
+            pledgeSection.classList.replace("border-t-0", "border-t-2");
         }
     }
 })
 
+// GOT IT (SUCCESS MODAL)
 closeModalSuccess.addEventListener("click", ()=> {
     closeModalSuccess.parentElement.classList.add("hidden");
     overlay.classList.add("hidden");
-    menuIcon.classList.add("z-20");
-    document.querySelector("header img").classList.add("z-20");
 })
