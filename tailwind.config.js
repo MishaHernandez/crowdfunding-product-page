615a30d6e1b718441ba57f25f8be6c157d92001a
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-15 18:26:34 -0500

    feat: Added counters in statistics section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  purge: [
-    "./dist/*.html",
-    "./dist/**/*.js",
-  ],
+  purge: {
+    content: ['./dist/**/*.html', './dist/**/*.js']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
